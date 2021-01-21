0cf01ca7e19cb29eacf1649636aaf6a5a8eca970
neo1 <tobby2002@gmailcom>
2021-01-21 21:54:01 +0900

    add ninja api urls

---
+++ app/api.py
@@ -0,0 +1,44 @@
+from datetime import date
+from ninja import Router
+from typing import List
+from pydantic import BaseModel
+from django.shortcuts import get_object_or_404
+from .models import Event
+
+
+from ninja import NinjaAPI
+
+api = NinjaAPI()
+
+
+@api.get("/add")
+async def add(request, a: int, b: int):
+    return {"result": a + b}
+
+router = Router()
+
+
+class EventSchema(BaseModel):
+    title: str
+    start_date: date
+    end_date: date
+
+    class Config:
+        orm_mode = True
+
+
+@router.post("/create")
+def create_event(request, event: EventSchema):
+    Event.objects.create(**event.dict())
+    return event
+
+
+@router.get("", response=List[EventSchema])
+def list_events(request):
+    return list(Event.objects.all())
+
+
+@router.get("/{id}", response=EventSchema)
+def get_event(request, id: int):
+    event = get_object_or_404(Event, id=id)
+    return event
--- app/settings.py
+++ app/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django_dashboard_bs4',
     'django_filters',
     "django_apscheduler",
+    "app",
 ]
 
 
