81d4ce57a4313f4e1d87145848259bb23bb9f62b
neo1 <tobby2002@gmailcom>
2021-01-22 00:50:25 +0900

    add api search

--- app/settings.py
+++ app/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'django_filters',
     "django_apscheduler",
     "app",
+
 ]
 
 
--- app/urls.py
+++ app/urls.py
@@ -6,6 +6,7 @@ from django.conf.urls import url, include
 from rest_framework.routers import DefaultRouter
 from rest_framework.schemas import get_schema_view
 from rest_framework.documentation import include_docs_urls
+from .api import api
 
 router = DefaultRouter()
 router.register(r'match', views.MatchViewSet)
@@ -16,7 +17,8 @@ schema_view = get_schema_view(title='Bookings API',
                 description='An API to book matches or update odds.')
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/', include(router.urls)),
+    path('api2/', include(router.urls)),
+    path("api/", api.urls),  # <---------- !
     url(r'^api/indexing/solr/(?P<collection_name>[\w.@+-]+)$', include('django_solr_rest_apis.urls')),
     url(r'^$', include('django_dashboard_bs4.urls')),
     path('schema/', schema_view),
